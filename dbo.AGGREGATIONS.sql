07cc4e288832929ba3e869ebf82a387027a61686
Tjitjila Netshiungani <tsmokono@gmail.com>
2026-03-03 07:00:05 +0000

    Push v3

--- dbo.AGGREGATIONS.sql
+++ dbo.AGGREGATIONS.sql
@@ -1,27 +1,43 @@
 select * from dbo.[AGGREGATIONS]
+where property_price < '2000000'
+where province = 'Gauteng' and city = 'Midrand' 
+
 
 SECTION 1 – COUNT Aggregations (10 Questions)
 
 --1. How many total properties are in the database?
-select * from dbo.[AGGREGATIONS]
+SELECT COUNT(PROPERTY_ID)
+FROM dbo.[AGGREGATIONS]
 
 --2. How many properties are listed in each province?
-SELECT COUNT(Property_ID) AS Total_Properties
-FROM dbo.[AGGREGATIONS]
+SELECT ([PROVINCE]),
 
-SELECT COUNT(*) AS Total_Properties
+COUNT ([PROPERTY_ID]) AS [No_Of_Properties]
 FROM dbo.[AGGREGATIONS]
+GROUP BY [Province]
 
 --3. How many properties are listed in each city?
-SELECT COUNT(Property_ID) AS Total_Properties
+SELECT (CITY),
+
+COUNT (PROPERTY_ID) AS No_Of_Properties
 FROM dbo.[AGGREGATIONS]
+GROUP BY CITY
 
 
 --4. How many properties have more than 2 bedrooms?
+SELECT COUNT(*) AS No_of_Properties_with_2plus_Bedrooms
+FROM dbo.[AGGREGATIONS]
+Where BEDROOMS>2
 
 --5. How many properties have 2 or more bathrooms?
+Select count (*) As No_of_Properties_with_2plus_Bathrooms
+from dbo.AGGREGATIONS
+where bathrooms>=2
 
 --6. How many properties have parking for at least 2 cars?
+Select count (*) As No_of_Properties_with_2plus_Parking
+from dbo.AGGREGATIONS
+where Parking>=2
 
 --7. How many properties are priced above R3,000,000?
 
